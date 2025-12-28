ed16c28e34eada130b3cb527b8f1c2ab4281bbb4
Ugur Rahman Arat <ugurrahmanarat@gmail.com>
2025-12-28 18:52:16 +0300

    "Adım 1 İsim Eklendi"

---
+++ SAT242516042/Models/MyDbModels/IslemTuru.cs
@@ -0,0 +1,11 @@
+﻿namespace MyDbModels
+{
+    // Veritabanı işlemlerinde standart sağlamak için kullanılan Enum yapısı
+    public enum IslemTuru
+    {
+        LISTE,
+        KAYDET,
+        SIL,
+        DETAY
+    }
+}
